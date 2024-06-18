840fbf51705560c8a2dc5d63f714ffe91b5d1ecf
Joao Paulo Costeira <47594182+jpcosteira@users.noreply.github.com>
2024-06-18 13:25:25 +0100

    Update external1.py

--- src/external1.py
+++ src/external1.py
@@ -1,3 +1,7 @@
+# Python module with the processing to be done in the component
+#input (datafile)- bytes with the input mat file
+#return (f.getvalue()) - bytes with the output mat file
+
 import numpy as np
 import io
 import cv2
@@ -5,10 +9,10 @@ from scipy.io import loadmat, savemat
 
 # returns numpy arrays with image annotated, xy coordinates of sift and descriptors
 def calling_function(datafile):
-    
+    #Load the mat file using scipy.io.loadmat
     mat_data=loadmat(io.BytesIO(datafile))
-    print(mat_data.keys())
-    print(mat_data["im"].shape)
+#    print(mat_data.keys())
+#    print(mat_data["im"].shape)
     im2=mat_data["im"]
     gray= cv2.cvtColor(im2,cv2.COLOR_BGR2GRAY)
     sift = cv2.SIFT_create()
@@ -33,4 +37,3 @@ def calling_function(datafile):
     f=io.BytesIO()
     savemat(f,{"im":im2,"kp":keypoints_array,"desc":descriptor})
     return f.getvalue()
-
